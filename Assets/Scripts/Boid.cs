5f5b27a10b76d172989edef5e37a2fbfb7aa5b3d
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2017-03-30 21:48:04 -0400

    Init flocking demo

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    // Public variables
+
+    // Private variables
+    private Rigidbody rb;
+    private List<GameObject> neighbours;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(Random.value, Random.value, Random.value);
+        neighbours = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 alignmentVec = Vector3.zero;
+        Vector3 cohesionVec = Vector3.zero;
+        Vector3 seperationVec = Vector3.zero;
+
+        foreach (GameObject o in neighbours)
+        {
+            if(o.gameObject != this.gameObject)
+            {
+                // Alignment
+                alignmentVec += o.GetComponent<Rigidbody>().velocity;
+
+                // Cohesion
+                cohesionVec += o.transform.position;
+
+                // Separation
+                seperationVec += this.transform.position - o.transform.position;
+            }
+        }
+
+        // Combine everything
+        alignmentVec /= neighbours.Count;
+        alignmentVec.Normalize();
+
+        cohesionVec /= neighbours.Count;
+        cohesionVec = new Vector3(cohesionVec.x - this.transform.position.x, cohesionVec.y - this.transform.position.y, cohesionVec.z - this.transform.position.z);
+        cohesionVec.Normalize();
+
+        seperationVec *= -1;
+        seperationVec.Normalize();
+
+        Vector3 v = rb.velocity; 
+        v += alignmentVec + cohesionVec + seperationVec;
+        v.Normalize();
+        rb.velocity = v;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Boid")
+        {
+            neighbours.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Boid")
+        {
+            neighbours.Remove(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/BoidController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController : MonoBehaviour {
+
+    public int numberOfBoids = 50;
+    public GameObject boid;
+
+	// Use this for initialization
+	void Start () {
+	    for(int i = 0; i < numberOfBoids; i++)
+        {
+            Instantiate(boid, new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)), Quaternion.identity);
+        }	
+	}
+}
